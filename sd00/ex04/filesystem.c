6e8a3a716711eac4c0d66099a644851baa2e5c37
Oleksandr Dudniak <sandoramix.dev@gmail.com>
2025-06-10 17:27:23 +0000

    ex04

---
+++ sd00/ex04/filesystem.c
@@ -0,0 +1,45 @@
+#include "filesystem.h"
+
+FSNode *create_file(const char *name, int size)
+{
+
+	FSNode *result = (FSNode *)malloc(sizeof(FSNode));
+
+	if (!result)
+		return (NULL);
+
+	result->name = strdup(name);
+	result->size = size;
+	result->children = NULL;
+	result->sibling = NULL;
+	return (result);
+}
+
+FSNode *create_folder(const char *name)
+{
+	return (create_file(name, 0));
+}
+
+void add_child(FSNode *parent, FSNode *child)
+{
+	if (!parent->children)
+	{
+		parent->children = child;
+		return;
+	}
+
+	FSNode *last_child = parent->children;
+	while (last_child->sibling)
+		last_child = last_child->sibling;
+	last_child->sibling = child;
+}
+
+FSNode *get_children(const FSNode *parent)
+{
+	return (parent->children);
+}
+
+FSNode *get_sibling(const FSNode *node)
+{
+	return (node->sibling);
+}
