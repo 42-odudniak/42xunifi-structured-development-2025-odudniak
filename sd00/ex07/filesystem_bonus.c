ea3840c8bb9c34b38226088215756ba4ec99c117
Oleksandr Dudniak <sandoramix.dev@gmail.com>
2025-06-10 20:12:16 +0000

    init ex07 & ex08

---
+++ sd00/ex07/filesystem_bonus.c
@@ -0,0 +1,11 @@
+#include "filesystem.h"
+
+int compute_total_size(FSNode *node)
+{
+}
+void print_structure(const FSNode *node, int indent)
+{
+}
+void free_filesystem(FSNode *node)
+{
+}
---
+++ sd00/ex08/password_validator.h
@@ -0,0 +1,8 @@
+#ifndef PASSWORD_VALIDATOR_H
+# define PASSWORD_VALIDATOR_H
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
