98d4b43729a5f52db5f4c828190bcdd7c0556dc3
Oleksandr Dudniak <sandoramix.dev@gmail.com>
2025-06-10 17:57:36 +0000

    ex05

---
+++ sd00/ex05/password_validator.h
@@ -0,0 +1,16 @@
+#ifndef PASSWORD_VALIDATOR_H
+#define PASSWORD_VALIDATOR_H
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum PwStatusEnum
+{
+	VALID,
+	INVALID
+}	PwStatus;
+
+PwStatus validate_password(const char *new_pw, const char *curr_pw);
+
+#endif
