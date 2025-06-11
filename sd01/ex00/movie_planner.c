26e5ab8fce8149891c6ff8e974d183b104c953e2
Oleksandr Dudniak <odudniak@student.42firenze.it>
2025-06-11 22:04:38 +0000

    ex00

--- sd01/ex00/movie_planner.c
+++ sd01/ex00/movie_planner.c
@@ -3,8 +3,12 @@
 struct Plan *create_movie_night_plan(void)
 {
 	struct Preferences *user_preferences = get_user_preferences();
+	if (!user_preferences)
+		return (NULL);
 
 	struct MovieList *movies = find_movies(user_preferences);
+	if (!movies)
+		return (NULL);
 
 	struct Plan *final_plan = build_plan(movies);
 
