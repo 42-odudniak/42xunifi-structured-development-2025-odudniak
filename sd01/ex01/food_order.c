028cf33c4f0b118277ab66de0a11beb6377059bf
Oleksandr Dudniak <odudniak@student.42firenze.it>
2025-06-11 22:10:10 +0000

    ex01

---
+++ sd01/ex01/food_order.c
@@ -0,0 +1,19 @@
+#include "food_order.h"
+
+int process_food_order(struct OrderRequest *request)
+{
+	const bool is_opened = check_restaurant_status(request) == 1;
+
+	struct OrderConfirmation *confirmation = NULL;
+	if (!is_opened)
+		confirmation = create_preorder_confirmation();
+	else
+		confirmation = create_standard_confirmation();
+
+	if (!confirmation)
+		return (0);
+
+	send_confirmation_notification(confirmation);
+
+	return (1);
+}
